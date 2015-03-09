194cab0b3275e3b19423b4e50a5a1aa1a38c0b2a
David-PC\David <david.lopez.castellote@gmail.com>
2015-03-09 12:48:00 +0100

    Commit 1 - David

---
+++ HolaMundo/src/holaMundo/HolaMundo.java
@@ -0,0 +1,20 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void holaMundo() {
+		System.out.println( "Hola mundo" );
+	}
+	
+	public static void main(String[] args) {
+		
+		HolaMundo hola = new HolaMundo();
+		hola.holaMundo();
+		
+	}
+
+}
