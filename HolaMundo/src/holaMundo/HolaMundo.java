791000566fff322d864b8ccafb70fdd19fa8787b
Xavi-nho <xavigarciam6@hotmail.com>
2015-03-09 13:33:14 +0100

    C

--- HolaMundo/src/holaMundo/HolaMundo.java
+++ HolaMundo/src/holaMundo/HolaMundo.java
@@ -10,6 +10,11 @@ public class HolaMundo {
 		System.out.println( "Hola mundo" );
 	}
 	
+	
+	public boolean pegarADavid(boolean si) {
+		return si;
+	}
+	
 	public static void main(String[] args) {
 		
 		HolaMundo hola = new HolaMundo();
